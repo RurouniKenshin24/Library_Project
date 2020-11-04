f1e80975761cc36a10872c94c6babbf8015220e4
sshukhratov <sshukhratov@gmail.com>
2020-11-03 22:02:06 -0600

    @Then step is added

--- src/test/java/com/libraryct/step_definitions/UserFilterDropdown_StepDefinitions.java
+++ src/test/java/com/libraryct/step_definitions/UserFilterDropdown_StepDefinitions.java
@@ -1,9 +1,15 @@
 package com.libraryct.step_definitions;
 
 import com.libraryct.pages.UserManagementPage;
+import com.libraryct.utils.BrowserUtils;
+import com.libraryct.utils.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.HashSet;
 
 public class UserFilterDropdown_StepDefinitions {
 
@@ -18,8 +24,23 @@ public class UserFilterDropdown_StepDefinitions {
 
 
 
-    @Then("librarian should be able to see all users on the table are Librarians")
-    public void librarian_should_be_able_to_see_all_users_on_the_table_are_librarians() {
+    @Then("librarian should be able to see all users on the table are {string}")
+    public void librarian_should_be_able_to_see_all_users_on_the_table_are_librarians(String string) {
+        userManagementPage.selectRecords("500");
+        BrowserUtils.wait(1); // wait until all elements are loaded
+
+        HashSet<String> userGroups = new HashSet<>(userManagementPage.getStringOfStudents()); // Set will helps us to eliminate all duplicates of "Librarian"
+
+        Driver.getDriver().findElement(By.xpath("//a[.='2']")).click(); // clicking to next page to get all users
+        BrowserUtils.wait(1); // wait until all elements are loaded
+
+        userGroups.addAll(userManagementPage.getStringOfStudents()); // if all objects are the same size of HashSet is still 1
+
+        if (userGroups.size() == 1){ // if size =! 1, it means all object are not the same
+            Assert.assertTrue(userGroups.contains(string));
+        }else {
+            Assert.fail();
+        }
 
     }
 }
--- src/test/resources/features/UserFilterDropdown.feature
+++ src/test/resources/features/UserFilterDropdown.feature
@@ -6,4 +6,4 @@ Feature: As an authorized user, the Librarian should be able to filter Users fro
   Scenario: filter table by users
     Given librarian is on the User Management page
     When librarian selects the "Librarian" from User Groups dropdown menu
-    Then librarian should be able to see all users on the table are Librarians
+    Then librarian should be able to see all users on the table are "Librarians"
